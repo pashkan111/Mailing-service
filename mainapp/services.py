694a6501e2b8ae93b8a5e77e02303959127f448d
pashkan111 <kozhevnikov0001@yandex.ru>
2022-03-01 18:35:07 +0300

    added client to interact with remote service

--- mainapp/services.py
+++ mainapp/services.py
@@ -1,5 +1,6 @@
 from . import models
 from django.db.models import Count
+import os, requests
 
 
 def get_statistic() -> dict:
@@ -13,3 +14,37 @@ def get_statistic() -> dict:
         messages.setdefault(mail.id, mail_message)
     return messages
     
+
+class ServiceClient:
+    """
+    Client to interact with remote service
+        which can send message to phone number
+    """
+    
+    def __init__(self, id):
+        service_url = os.environ.get('SERVICE_URL')
+        service_token = os.environ.get('SERVICE_TOKEN')
+        if service_url and service_token:
+            self.url = f'{service_url}/{id}'
+            self.token = service_token
+        else:
+            raise Exception
+    
+    def send_data(self, data: dict) -> bool:
+        response = requests.post(
+            url=self.url, 
+            data=data,
+            headers={
+                'Authorization': self.token
+            }
+        )
+        try:
+            response.raise_for_status()
+            return True
+        except Exception as e:
+            # add loging
+            print(str(e))
+            return False
+            
+            
+        
