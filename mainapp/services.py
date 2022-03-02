d8a8c19fbc62b6752de2eece03acaf0d06e5b61a
pashkan111 <kozhevnikov0001@yandex.ru>
2022-03-02 12:38:11 +0300

    created folder 'mailing_service' for external service

--- mainapp/services.py
+++ mainapp/services.py
@@ -1,6 +1,5 @@
 from . import models
 from django.db.models import Count
-import os, requests
 
 
 def get_statistic() -> dict:
@@ -13,36 +12,6 @@ def get_statistic() -> dict:
         mail_message = list(mail.mailings_messages.all().values('status').annotate(total=Count('id')))
         messages.setdefault(mail.id, mail_message)
     return messages
-    
 
-class ServiceClient:
-    """
-    Client to interact with remote service
-        which can send message to phone number
-    """
-    
-    def __init__(self, id):
-        service_url = os.environ.get('SERVICE_URL')
-        service_token = os.environ.get('SERVICE_TOKEN')
-        if service_url and service_token:
-            self.url = f'{service_url}/{id}'
-            self.token = service_token
-        else:
-            raise Exception
-    
-    def send_data(self, data: dict) -> bool:
-        response = requests.post(
-            url=self.url, 
-            data=data,
-            headers={'Authorization': self.token}
-        )
-        try:
-            response.raise_for_status()
-            return True
-        except Exception as e:
-            # add loging
-            print(str(e))
-            return False
             
             
-        
