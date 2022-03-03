ec4396e3278865270b55f7d343f02c64286333bc
pashkan111 <kozhevnikov0001@yandex.ru>
2022-03-03 11:21:22 +0300

    add logic to check_mailing_time task

--- mainapp/services.py
+++ mainapp/services.py
@@ -43,14 +43,50 @@ def create_object_to_send(id: int, tag: str) -> List[Dict]:
         data.append(obj)
     return data
 
-def check_time(id: int):
+
+def check_time(id: int) -> bool:
     """
     Gets the mailing by id and checks:
-        if time start < current time < finish time
+        if time start < current time < finish time returns True
     """
     mailing = models.Mailing.objects.get(id=id)
     time_start = mailing.date_start
     time_finish = mailing.date_finish
     current_datetime = datetime.datetime.now()
     if time_start < current_datetime < time_finish:
-        print('start')
+        return True
+    return False
+
+
+class MailingData:
+    def __init__(self, id: int, tag: str):
+        self.tag = tag
+        self.id = id
+        self.data = self._create_object_to_send()
+        
+    def _get_phones_for_mailing(self) -> List[Dict]:
+        phones = models.Client.objects.filter(tag=self.tag).values('phone')
+        return list(phones)
+
+    def _get_text_message(self) -> dict:
+        try:
+            message = models.Mailing.objects.get(id=self.id)
+            return {"text": message.text}
+        except models.Mailing.DoesNotExist:
+            # TODO add logging
+            print('exception')
+        
+    def _create_object_to_send(self) -> List[Dict]:
+        """
+        Creates a list of objects that will be sent to mailing service
+        """
+        message = self._get_text_message()
+        phones = self._get_phones_for_mailing()
+        data = []
+        for obj in phones:
+            obj.setdefault('text', message)
+            obj.setdefault('id', id)
+            data.append(obj)
+        return data
+        
+        
--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -1,10 +1,17 @@
 from celery import shared_task
+from .services import check_time, MailingData
+from mainapp.mailing_service.client import ServiceClient
 
 
 @shared_task
-def check_mailing_time():
+def check_mailing_time(id: int, tag: str):
     """
-    Got run right after creating new mailing
-    and check the time
+    Got run right after creating new mailing.
+    Checks the time of mailing and if is satisfies condition - 
+    sends data
     """
-    pass
+    is_started = check_time(id, tag)
+    if is_started:
+        data = MailingData()
+        client = ServiceClient(data)
+        client.send_data()
