75b54025c84c7aa45f8e564b85f3f01cca447e53
pashkan111 <kozhevnikov0001@yandex.ru>
2022-03-09 03:29:53 +0300

    Created test for checking routes for create, update and delete clients

---
+++ mainapp/tests/tests_api/test_clients.py
@@ -0,0 +1,86 @@
+from rest_framework.test import APIClient
+from django.test import TestCase
+from mainapp import models
+from rest_framework import status
+from mainapp.api import serializers
+import json
+
+
+class TestCase1(TestCase):
+    
+    def setUp(self) -> None:
+        self.client = APIClient()
+        
+    @classmethod
+    def setUpTestData(cls):
+        client1 = models.Client.objects.create(
+            phone="9565453670",
+            code="+7",
+            tag="clients1",
+            timezone="Moscow"
+        )
+        client2 = models.Client.objects.create(
+            phone="9565454440",
+            code="+7",
+            tag="clients1",
+            timezone="Moscow"
+        )
+        client3 = models.Client.objects.create(
+            phone="9565444670",
+            code="+8",
+            tag="clients2",
+            timezone="Asia"
+        )
+    
+    def test_get_clients(self):
+        clients = models.Client.objects.all()
+        response = self.client.get('/api/clients')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.json(), serializers.ClientSerializer(
+                clients, many=True
+                ).data
+        )
+        
+    def test_post_client(self):
+        body = {
+            "phone": "888888",
+            "code": "+7",
+            "tag": "clients2",
+            "timezone": "Moscow"
+        }
+        response = self.client.post(
+            '/api/clients', body, format='json'
+        )
+        clients_count = models.Client.objects.count()
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(clients_count, 4)
+        
+    def test_update_client(self):
+        client_pk = models.Client.objects.first().pk
+        body = {
+            "phone": "9999999",
+            "tag": "updated",
+        }
+        response = self.client.patch(
+            f'/api/clients/{client_pk}', body, format='json'
+        )
+        client_updated = models.Client.objects.get(pk=client_pk)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.json(), serializers.ClientSerializer(client_updated).data
+            )
+        
+    def test_delete_client(self):
+        client_pk = models.Client.objects.first().pk
+        response = self.client.delete(
+            f'/api/clients/{client_pk}', format='json'
+        )
+        clients_count = models.Client.objects.count()
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(clients_count, 2)
+        
